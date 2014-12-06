675a95df3dcc2580aecae377bb784bc3b96b4dbf
Kevin Herrera <kherrera16@gmail.com>
2014-12-05 18:11:53 -0600

    Second Commit

--- application.py
+++ application.py
@@ -1,88 +1,87 @@
 #coding: utf-8
 """Motor de Busqueda"""
 import re
-import shutil
 import os
 import sys
 import time
 import urllib
 
 #cont = 0
-class Motor(object):
+class _Motor(object):
+    def __init__(self):
+        self.pagina1 = ""
+        self.pagina2 = ""
+        self.palabra = ""
+        self.bus1 = ""
+        self.res1 = ""
+        self.cont1 = 0
+        self.bus2 = ""
+        self.res2 = ""
+        self.cont2 = 0
+        self.oportunidad = 0
 
     def busqueda(self):
-        RESPUESTA = "si"
-        while RESPUESTA != "no":
+        respuesta = "si"
+        while respuesta != "no":
 
-            print "Hola Bienvenido al\n                  ***** Motor de Busqueda ***** \n"
+            print "Hola Bienvenido al\n "
+            print "***** Motor de Busqueda ***** \n"
 
-            pagina1 = raw_input("Ingrese #1 URL: ")
+            self.pagina1 = raw_input("Ingrese #1 URL: ")
+            self.pagina2 = raw_input("Ingrese #2 URL: ")
+            self.palabra = raw_input("Ingrese palabra a Buscar: ")
 
+            self.bus1 = urllib.urlopen(self.pagina1)
+            self.res1 = self.bus1.read()
+            self.cont1 = len(re.findall(self.palabra, self.res1))
 
-            pagina2 = raw_input("Ingrese #2 URL: ")
+            self.bus2 = urllib.urlopen(self.pagina2)
+            self.res2 = self.bus2.read()
+            self.cont2 = len(re.findall(self.palabra, self.res2))
 
-            palabra = raw_input("Ingrese Palabra a Buscar: ")
+            if self.cont1 > self.cont2:
+                print "\nTu palabra: " + self.palabra
+                print "Aparece en URL #1: " + str(self.cont1) + " veces"
+                print "Aparece en URL #2: " + str(self.cont2) + " veces"
+                print "Página Recomendada: " + self.pagina1 + "\n"
 
-            bus1 = urllib.urlopen (pagina1)
-            res1 = bus1.read()
-            cont1 = len(re.findall(palabra, res1))
+            elif self.cont2 > self.cont1:
+                print "\nTu palabra: " + self.palabra
+                print "Aparece en URL #1: " + str(self.cont1) + " veces"
+                print "Aparece en URL #2: " + str(self.cont2) + " veces"
+                print "Página Recomendada: " + self.pagina2 + "\n"
 
-            bus2 = urllib.urlopen (pagina2)
-            res2 = bus2.read()
-            cont2 = len(re.findall(palabra, res2))
-
-            self.pagina1 = pagina1
-            self.pagina2 = pagina2
-            self.palabra = palabra
-            self.bus1 = bus1
-            self.res1 = res1
-            self.cont1 = cont1
-            self.bus2 = bus2
-            self.res2 = res2
-            self.cont2 = cont2
-
-
-            if cont1 > cont2:
-                print "\nTu Palabra: "+palabra
-                print "Aparece en URL #1: "+str(cont1)+" veces"
-                print "Aparece en URL #2: "+str(cont2)+" veces"
-                print "Página Recomendada: "+pagina1 +"\n"
-
-            elif cont2 > cont1:
-                print "\nTu Palabra: "+palabra
-                print "Aparece en URL #1: "+str(cont1)+" veces"
-                print "Aparece en URL #2: "+str(cont2)+" veces"
-                print "Página Recomendada: "+pagina2 +"\n"
-            elif (cont1 > 0) and (cont2 > 0) and cont1 == cont2:
-                print "Tu Palabra: "+palabra
-                print "Aparece en URL #1: "+str(cont1)+" veces"
-                print "Aparece en URL #2: "+str(cont2)+" veces"
+            elif (self.cont1 > 0) and (self.cont2 > 0) and self.cont1 == self.cont2:
+                print "Tu palabra: " + self.palabra
+                print "Aparece en URL #1: " + str(self.cont1) + " veces"
+                print "Aparece en URL #2: " + str(self.cont2) + " veces"
                 print "No hay Página Recomendada"
-            elif cont1 == 0 and cont2 == 0:
-                print "\nPalabra No Encontrada"
+
+            elif self.cont1 == 0 and self.cont2 == 0:
+                print "\npalabra No Encontrada"
                 print "No hay Página Recomendada\n"
 
-            while RESPUESTA != "no":
-                RESPUESTA = raw_input("Desea realizar otra busqueda si/no: \n")
-                RESPUESTA = RESPUESTA.lower()
+            while respuesta != "no":
+                respuesta = raw_input("Desea realizar otra busqueda si/no: \n")
+                respuesta = respuesta.lower()
 
                 try:
-                    RESPUESTA = float(RESPUESTA)
-                    RESPUESTA = int(RESPUESTA)
+                    respuesta = float(respuesta)
+                    respuesta = int(respuesta)
                     print "Error Desea realizar otra busqueda: \n"
                 except(RuntimeError, NameError, ValueError):
-                    RESPUESTA = RESPUESTA
+                    respuesta = respuesta
 
-                if RESPUESTA == "si":
-                    pagina1 = ""
-                    pagina2 = ""
-                    palabra = ""
+                if respuesta == "si":
+                    self.pagina1 = ""
+                    self.pagina2 = ""
+                    self.palabra = ""
                     time.sleep(1)
                     os.system("clear")
 
                     break
 
-                elif RESPUESTA == "no":
+                elif respuesta == "no":
                     break
 
                 else:
@@ -91,48 +90,49 @@ class Motor(object):
         os.system("clear")
         return
 
-def Menu():
-    OP = 0
-    while True:
-        while (OP != 1)or(OP != 2):
+    def menu(self):
+        self.oportunidad = 0
+        while True:
+            while (self.oportunidad != 1) or (self.oportunidad != 2):
 
-            print"                    ***** Motor de Busqueda © ***** \n"
-            print "1. Ingresar Busqueda"
-            print "2. Salir"
+                print"                    ***** Motor de Busqueda © ***** \n"
+                print "1. Ingresar Busqueda"
+                print "2. Salir"
 
-            OP = raw_input("Ingresa una opción: ")
-
-            try:
-                OP = int(OP)
-                if OP > 0 and OP <= 2:
-                    break
-                else:
+                self.oportunidad = raw_input("Ingresa una opción: ")
+                try:
+                    self.oportunidad = int(self.oportunidad)
+                    if self.oportunidad > 0 and self.oportunidad <= 2:
+                        break
+                    else:
+                        print "Ingrese opción válida\n"
+                        time.sleep(1)
+                        os.system("clear")
+                except(RuntimeError, TypeError, NameError, ValueError):
                     print "Ingrese opción válida\n"
                     time.sleep(1)
                     os.system("clear")
-            except(RuntimeError, TypeError, NameError, ValueError):
-                print "Ingrese opción válida\n"
-                time.sleep(1)
+
+            if self.oportunidad == 1:
+                inicio = _Motor()
+                inicio.busqueda()
+
+            if self.oportunidad == 2:
+                print "Saliendo ..."
+                time.sleep(2)
                 os.system("clear")
+                print"                            ***** Motor de Busqueda © ***** \n"
+                print u"                      Third Generation Grupo: KAR-FER :\n"
+                print u"                                 **************"
+                print u"                                 Kevin Herrera"
+                print u"                                 **************\n"
+                print u"                          KAR_KO,INDUSTRIS Copright ®"
+                time.sleep(1)
+                sys.exit()
+        self.oportunidad = 0
+        return
 
-        if OP == 1:
-            inicio = Motor()
-            inicio.busqueda()
-
-        if OP == 2:
-            print "Saliendo ..."
-            time.sleep(2)
-            os.system("clear")
-            print"                            ***** Motor de Busqueda © ***** \n"
-            print u"                      Third Generation Grupo: KAR-FER :\n"
-            print u"                                 **************"
-            print u"                                 Kevin Herrera"
-            print u"                                 **************\n"
-            print u"                          KAR_KO,INDUSTRIS Copyright ®"
-            time.sleep(1)
-            sys.exit()
-    OP = 0
-    return
-Menu() # Llamo a mi funcion Menu
+START = _Motor()
+START.menu()
 
 
